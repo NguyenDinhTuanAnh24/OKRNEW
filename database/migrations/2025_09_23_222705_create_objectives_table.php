bf3ee5afee563d36aba3930727bfbb84fdda6268
Nguyntanh <nguyntanh2k5@gmail.com>
2025-09-30 08:54:46 +0700

    update db

--- database/migrations/2025_09_23_222705_create_objectives_table.php
+++ database/migrations/2025_09_23_222705_create_objectives_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
             $table->string('level')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->string('description')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
             $table->string('status')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
-            $table->decimal('progress_percent', 5, 2)->default(0);
-            $table->foreignId('user_id')->constrained('users','user_id')->cascadeOnDelete();
-            $table->foreignId('cycle_id')->constrained('cycles','cycle_id')->cascadeOnDelete();
+            $table->decimal('progress_percent', 5, 2)->nullable()->default(0);
+            $table->foreignId('user_id')->nullable()->constrained('users','user_id')->cascadeOnDelete();
+            $table->foreignId('cycle_id')->nullable()->constrained('cycles','cycle_id')->cascadeOnDelete();
             $table->timestamps();
         });
     }
